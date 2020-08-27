a3336c408532f93be177a28b172d60f1f8a34bf0
AugutusPereghin <agp5191@psu.edu>
2020-08-27 03:52:46 +0000

    fixed formatting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 name = input("Enter your name: ")
 print("Hello, " + name + "!")
-age = input("Enter your age: ")
-print("You will be " + str(int(age)+1) + " next year")
+age = int(input("Enter your age: "))
+print("You will be " + str(age+1) + " next year")
